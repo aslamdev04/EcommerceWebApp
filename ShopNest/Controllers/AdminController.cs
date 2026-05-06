2a628eaab6157274a7b136eccb77f59d76f62608
aslamdev04 <aslamkhan86172@gmail.com>
2026-05-06 14:28:09 +0530

    InitialCommete

---
+++ ShopNest/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopNest.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult user_mgmt()
+        {
+            return View();
+        }
+        public IActionResult Cat_mgmt()
+        {
+            return View();
+        }
+        public IActionResult Product_mon()
+        {
+            return View();
+        }
+        public IActionResult Order_mgmt()
+        {
+            return View();
+        }
+        public IActionResult Payment_mgmt()
+        {
+            return View();
+        }
+        public IActionResult Offer_noti()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+     
+    }
+}
---
+++ ShopNest/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopNest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Browse()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult MyOrders()
+        {
+            return View();
+        }
+        public IActionResult Order_Track()
+        {
+            return View();
+           
+        }
+        public IActionResult Reviews()
+        {
+            return View();
+           
+        }
+        public IActionResult Notifications()
+        {
+            return View();
+           
+        }
+        public IActionResult Payment()
+        {
+            return View();
+           
+        }
+  
+    }
+}
